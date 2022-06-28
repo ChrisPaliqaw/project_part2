9731719efbe65c8a44489b5de2e0b88f69763477
Chris Paliqaw <christophomos@gmail.com>
2022-06-28 08:34:31 +0000

    Commented out pre_approach from cmake to debug elevator publishing

---
+++ include/project_part2/pre_approach.hpp
@@ -0,0 +1,92 @@
+#ifndef PREAPPROACH_HPP_
+#define PREAPPROACH_HPP_
+
+#include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "project_part2/visibility_control.h"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "std_msgs/msg/detail/empty__struct.hpp"
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+namespace project_part2 {
+
+class PreApproach : public rclcpp::Node {
+public:
+  COMPOSITION_PUBLIC
+  explicit PreApproach(
+                          std::string scan_topic = "scan",
+                          std::string odom_topic = "odom",
+                          std::string cmd_vel_topic = "robot/cmd_vel",
+                          std::string elevator_down_topic = "elevator_down",
+                          std::string elevator_up_topic = "elevator_up");
+  static geometry_msgs::msg::Vector3 euler_from_quaternion(tf2::Quaternion q);
+
+  static constexpr int kFrontScanRange = 540;
+  static constexpr double kCloseWallDistance = 0.33;
+  static constexpr double kCloseCartDistance = 0.2;
+  static constexpr double kLinearVelocity = 0.08;
+  static constexpr double kGazeboLinearVelocity = 0.08;
+  static constexpr double kLeftAngularVelocity = 0.2;
+  static constexpr double kRightAngularVelocity = -kLeftAngularVelocity;
+  static const double kGoalAngularDisplacement;
+  static constexpr double kTurnFuzz = 0.1; // Precision of turn +-
+  static const std::string kIsGazeboParameter;
+
+  enum class PreApproachState {
+    forward_01,
+    stop_forward_01,
+    set_turn,
+    turn,
+    stop_turn,
+    forward_02,
+    stop_forward_02,
+    elevator_up,
+    stop
+  };
+
+private:
+  const std::string scan_topic;
+  const std::string odom_topic;
+  const std::string cmd_vel_topic;
+  const std::string elevator_up_topic;
+  const std::string elevator_down_topic;
+  // Set inside the constructor and never changed, but can't
+  // be declared constant because we must get the parameter is_gazebo in order
+  // to set it correctly
+  double linear_velocity_;
+  PreApproachState state_;
+  geometry_msgs::msg::Vector3
+      goal_turn_v3_; // Goal: odom pose z rotation when turning toward cart
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr
+      laser_subscription_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr elevator_up_publisher_;
+  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr elevator_down_publisher_;
+  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr message);
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr message);
+
+  bool is_gazebo_;
+  std::mutex state_mutex_;
+  std::shared_ptr<geometry_msgs::msg::Twist> twist_;
+  std::shared_ptr<std_msgs::msg::Empty> empty_;
+  void stop();
+  void turn();
+  void forward();
+  void elevator_up();
+  void publish_twist();
+  static std::string state_string(PreApproachState state);
+  static double magnitude(geometry_msgs::msg::Vector3 v3);
+  static bool is_stopped(geometry_msgs::msg::Vector3 v3);
+  bool is_buffer_stop_state() const;
+  void log_state_verbose() const;
+  void log_state() const;
+};
+} // namespace project_part2
+#endif // PREAPPROACH_HPP_
---
+++ src/pre_approach.cpp
@@ -0,0 +1,460 @@
+#include <functional>
+#include <rclcpp/rclcpp.hpp>
+#include <mutex>
+#include <string>
+#include <memory>
+#include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "geometry_msgs/msg/detail/vector3__struct.hpp"
+#include "../include/project_part2/pre_approach.hpp"
+#include "rclcpp/logging.hpp"
+#include "rcutils/logging.h"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "geometry_msgs/msg/vector3.hpp"
+#include "std_msgs/msg/empty.hpp"
+
+
+/*
+ros2 interface show sensor_msgs/msg/LaserScan
+# Single scan from a planar laser range-finder
+#
+# If you have another ranging device with different behavior (e.g. a sonar
+# array), please find or create a different message, since applications
+# will make fairly laser-specific assumptions about this data
+
+std_msgs/Header header # timestamp in the header is the acquisition time of
+                             # the first ray in the scan.
+                             #
+                             # in frame frame_id, angles are measured around
+                             # the positive Z axis (counterclockwise, if Z is up)
+                             # with zero angle being forward along the x axis
+
+float32 angle_min            # start angle of the scan [rad]
+float32 angle_max            # end angle of the scan [rad]
+float32 angle_increment      # angular distance between measurements [rad]
+
+float32 time_increment       # time between measurements [seconds] - if your scanner
+                             # is moving, this will be used in interpolating position
+                             # of 3d points
+float32 scan_time            # time between scans [seconds]
+
+float32 range_min            # minimum range value [m]
+float32 range_max            # maximum range value [m]
+
+float32[] ranges             # range data [m]
+                             # (Note: values < range_min or > range_max should be discarded)
+float32[] intensities        # intensity data [device-specific units].  If your
+                             # device does not provide intensities, please leave
+                             # the array empty.
+user:~$
+*/
+
+/*
+user:~$ ros2 interface show geometry_msgs/msg/Twist
+# This expresses velocity in free space broken into its linear and angular parts.
+
+Vector3  linear
+Vector3  angular
+*/
+
+/*
+user:~$ ros2 interface show geometry_msgs/msg/Vector3
+# This represents a vector in free space.
+
+# This is semantically different than a point.
+# A vector is always anchored at the origin.
+# When a transform is applied to a vector, only the rotational component is applied.
+
+float64 x
+float64 y
+float64 z
+*/
+
+/*
+$ ros2 interface show nav_msgs/msg/Odometry
+# This represents an estimate of a position and velocity in free space.
+# The pose in this message should be specified in the coordinate frame given by header.frame_id
+# The twist in this message should be specified in the coordinate frame given by the child_frame_id
+
+# Includes the frame id of the pose parent.
+std_msgs/Header header
+
+# Frame id the pose points to. The twist is in this coordinate frame.
+string child_frame_id
+
+# Estimated pose that is typically relative to a fixed world frame.
+geometry_msgs/PoseWithCovariance pose
+
+# Estimated linear and angular velocity relative to child_frame_id.
+geometry_msgs/TwistWithCovariance twist
+*/
+
+/*
+$ ros2 interface show geometry_msgs/msg/PoseWithCovariance
+# This represents a pose in free space with uncertainty.
+
+Pose pose
+
+# Row-major representation of the 6x6 covariance matrix
+# The orientation parameters use a fixed-axis representation.
+# In order, the parameters are:
+# (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)
+float64[36] covariance
+*/
+
+/*
+user:~/ros2_ws$ ros2 interface show geometry_msgs/msg/Pose
+# A representation of pose in free space, composed of position and orientation.
+
+Point position
+Quaternion orientation
+*/
+
+/*
+user:~/ros2_ws$ ros2 interface show geometry_msgs/msg/Quaternion
+# This represents an orientation in free space in quaternion form.
+
+float64 x 0
+float64 y 0
+float64 z 0
+float64 w 1
+*/
+
+using std::placeholders::_1;
+
+namespace project_part2
+{
+PreApproach::PreApproach(
+        std::string scan_topic,
+        std::string odom_topic,
+        std::string cmd_vel_topic,
+        std::string elevator_up_topic,
+        std::string elevator_down_topic):
+
+
+    Node("pre_approach_node"),
+    scan_topic(scan_topic),
+    cmd_vel_topic(cmd_vel_topic),
+    elevator_up_topic(elevator_up_topic),
+    elevator_down_topic(elevator_down_topic),
+    state_(PreApproachState::forward_01)
+{
+    this->declare_parameter<bool>(kIsGazeboParameter, false);
+    this->get_parameter(kIsGazeboParameter, is_gazebo_);
+    RCLCPP_INFO_STREAM(get_logger(), "is_gazebo = " << (is_gazebo_ ? "true" : "false"));
+    linear_velocity_ = (is_gazebo_ ? kGazeboLinearVelocity : kLinearVelocity);
+
+    laser_subscription_ = create_subscription<sensor_msgs::msg::LaserScan>(
+        scan_topic, 10, std::bind(&PreApproach::scan_callback, this, _1));
+
+    odom_subscription_ = create_subscription<nav_msgs::msg::Odometry>(
+        odom_topic, 10, std::bind(&PreApproach::odom_callback, this, _1));
+
+    cmd_vel_publisher_ = create_publisher<geometry_msgs::msg::Twist>(cmd_vel_topic, 10);
+
+    elevator_up_publisher_ = create_publisher<std_msgs::msg::Empty>(elevator_up_topic, 10);
+    elevator_down_publisher_ = create_publisher<std_msgs::msg::Empty>(elevator_down_topic, 10);
+
+
+    RCLCPP_INFO_STREAM(this->get_logger(), "Create PreApproach");
+
+    twist_ = std::make_shared<geometry_msgs::msg::Twist>();
+    empty_ = std::make_shared<std_msgs::msg::Empty>();
+    forward();
+
+    auto ret = rcutils_logging_set_logger_level(
+        get_logger().get_name(), RCUTILS_LOG_SEVERITY_INFO);
+    if (ret != RCUTILS_RET_OK) {
+        RCLCPP_ERROR(get_logger(), "Error setting severity: %s", rcutils_get_error_string().str);
+        rcutils_reset_error();
+    }
+
+    log_state();
+}
+
+// Code adapted from https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+geometry_msgs::msg::Vector3 PreApproach::euler_from_quaternion(tf2::Quaternion q)
+{
+    geometry_msgs::msg::Vector3 v3;
+
+    // roll (x-axis rotation)
+    double sinr_cosp = 2 * (q.w() * q.x() + q.y() * q.z());
+    double cosr_cosp = 1 - 2 * (q.x() * q.x() + q.y() * q.y());
+    double roll;
+    roll = std::atan2(sinr_cosp, cosr_cosp);
+
+    // pitch (y-axis rotation)
+    double sinp = 2 * (q.w() * q.y() - q.z() * q.x());
+    double pitch;
+    if (std::abs(sinp) >= 1)
+        pitch = std::copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+    else
+        pitch = std::asin(sinp);
+
+    // yaw (z-axis rotation)
+    double siny_cosp = 2 * (q.w() * q.z() + q.x() * q.y());
+    double cosy_cosp = 1 - 2 * (q.y() * q.y() + q.z() * q.z());
+    double yaw;
+    yaw = std::atan2(siny_cosp, cosy_cosp);
+
+    v3.x = roll;
+    v3.y = pitch;
+    v3.z = yaw;
+
+    return v3;
+}
+
+double PreApproach::magnitude(geometry_msgs::msg::Vector3 v3)
+{
+    return sqrt(v3.x*v3.x + v3.y*v3.y + v3.z*v3.z);
+}
+
+bool PreApproach::is_buffer_stop_state () const
+{
+    switch (state_)
+    {
+    case PreApproachState::stop_forward_01:
+        [[fallthrough]];
+    case PreApproachState::stop_turn:
+        [[fallthrough]];
+    case PreApproachState::stop_forward_02:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool PreApproach::is_stopped(geometry_msgs::msg::Vector3 v3)
+{
+    return abs(magnitude(v3)) <= kTurnFuzz;
+}
+
+void PreApproach::odom_callback(const nav_msgs::msg::Odometry::SharedPtr message)
+{
+    std::lock_guard<std::mutex> lock(state_mutex_);
+
+    // Only turning behavior is determined by odom
+    switch (state_)
+    {
+    case PreApproachState::stop_forward_01:
+        [[fallthrough]];
+    case PreApproachState::set_turn:
+        [[fallthrough]];
+    case PreApproachState::turn:
+        [[fallthrough]];
+    case PreApproachState::stop_turn:
+        [[fallthrough]];
+    case PreApproachState::stop_forward_02:
+        break;
+    default:
+        return;
+    }
+
+    tf2::Quaternion tf2_quaternion;
+    // Convert geometry_msgs::msg::Quaternion to tf2::Quaternion
+    tf2::convert(message->pose.pose.orientation, tf2_quaternion);
+
+    geometry_msgs::msg::Vector3 current_rotation_v3 = euler_from_quaternion(tf2_quaternion);
+
+    if (is_buffer_stop_state())
+    {
+        bool stopped = is_stopped(message->twist.twist.angular) && is_stopped(message->twist.twist.linear);
+        if (stopped)
+        {
+            switch (state_) {
+            case PreApproachState::stop_forward_01:
+                state_ = PreApproachState::set_turn;
+                RCLCPP_INFO_STREAM(get_logger(), "SET TURN IN THE PROPER PLACE");
+                log_state();
+                break;
+            case PreApproachState::stop_turn:
+                state_ = PreApproachState::forward_02;
+                forward();
+                log_state();
+                break;
+            case PreApproachState::stop_forward_02:
+                state_ = PreApproachState::elevator_up;
+
+                elevator_up();
+                log_state();
+                return;
+                /*
+                state_ = PreApproachState::stop;
+                stop();
+                log_state();
+                */
+                break;
+            default:
+                RCLCPP_ERROR_STREAM(get_logger(), "Unexpected state: " << state_string(state_));
+            }
+        }
+    }
+
+    if (state_ == PreApproachState::set_turn) {
+        RCLCPP_INFO_STREAM(get_logger(), "SETTING TURN");
+        tf2::Quaternion q_rot;
+        // Rotate the current pose by 90 degrees about Z to get our goal pose
+        q_rot.setRPY(0.0, 0.0, PreApproach::kGoalAngularDisplacement);
+        tf2::Quaternion goal_turn = q_rot * tf2_quaternion;
+        goal_turn.normalize();
+        goal_turn_v3_ = PreApproach::euler_from_quaternion(goal_turn);
+        state_ = PreApproachState::turn;
+        turn();
+        log_state();
+    }
+
+    else if ((state_ == PreApproachState::turn) && (abs(current_rotation_v3.z - goal_turn_v3_.z) <= kTurnFuzz))
+    {
+        state_ = PreApproachState::stop_turn;
+        stop();
+        log_state();
+    }
+    
+    publish_twist();
+}
+
+void PreApproach::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr message)
+{
+    // turning behavior is determined by odom
+    switch (state_)
+    {
+    case PreApproachState::stop_forward_01:
+        [[fallthrough]];
+    case PreApproachState::set_turn:
+        [[fallthrough]];
+    case PreApproachState::turn:
+        [[fallthrough]];
+    case PreApproachState::stop_turn:
+        [[fallthrough]];
+    case PreApproachState::stop_forward_02:
+        return;
+    default:
+        break;
+    }
+
+    std::lock_guard<std::mutex> lock(state_mutex_);
+    RCLCPP_DEBUG_STREAM(get_logger(), message->ranges[kFrontScanRange]);
+    switch (state_)
+    {
+        // Turning behavior is not determined by the scan
+        case PreApproachState::set_turn:
+            [[fallthrough]];
+        case PreApproachState::turn:
+            return;
+        case PreApproachState::forward_01:
+            if (message->ranges[kFrontScanRange] < kCloseWallDistance)
+            {
+                state_ = PreApproachState::stop_forward_01;
+                stop();
+                log_state();
+            }
+            break;
+        case PreApproachState::forward_02:
+            if (message->ranges[kFrontScanRange] < kCloseCartDistance)
+            {
+                state_ = PreApproachState::stop_forward_02;
+                stop();
+                log_state();
+            }
+            break;
+        default:
+            break;
+    }
+    publish_twist();
+}
+
+void PreApproach::stop()
+{
+    twist_->linear.x = 0;
+    twist_->linear.y = 0;
+    twist_->linear.z = 0;
+    twist_->angular.x = 0;
+    twist_->angular.y = 0;
+    twist_->angular.z = 0;
+}
+
+void PreApproach::turn()
+{
+    stop();
+    twist_->angular.z = this->kRightAngularVelocity;
+}
+
+void PreApproach::forward()
+{
+    stop();
+    twist_->linear.x = linear_velocity_;
+}
+
+void PreApproach::elevator_up()
+{
+    RCLCPP_INFO(get_logger(), "Publishing elevator_up");
+    elevator_up_publisher_->publish(*empty_);
+}
+
+void PreApproach::publish_twist()
+{
+    if (state_ != PreApproachState::stop)
+    {
+        cmd_vel_publisher_->publish(*twist_);
+    }
+}
+
+void PreApproach::log_state_verbose() const
+{
+    RCLCPP_DEBUG(get_logger(), state_string(state_));
+    RCLCPP_DEBUG_STREAM(get_logger(), "linear: (" << twist_->linear.x << ", "  << twist_->linear.y << ", "  << twist_->linear.z << ")");
+    RCLCPP_DEBUG_STREAM(get_logger(), "angular: (" << twist_->angular.x << ", "  << twist_->angular.y << ", "  << twist_->angular.z << ")");
+}
+
+void PreApproach::log_state() const
+{
+    RCLCPP_INFO(get_logger(), state_string(state_));
+    RCLCPP_INFO_STREAM(get_logger(), "linear: (" << twist_->linear.x << ", "  << twist_->linear.y << ", "  << twist_->linear.z << ")");
+    RCLCPP_INFO_STREAM(get_logger(), "angular: (" << twist_->angular.x << ", "  << twist_->angular.y << ", "  << twist_->angular.z << ")");
+}
+
+std::string PreApproach::state_string(PreApproachState state)
+{
+    std::string return_value;
+    switch (state)
+    {
+        case PreApproachState::forward_01:
+            return_value = "Moving toward the back wall";
+            break;
+        case PreApproachState::stop_forward_01:
+            return_value = "Stopping after moving toward the back wall";
+            break;
+        case PreApproachState::set_turn:
+            return_value = "Setting the turn speed";
+            break;
+        case PreApproachState::turn:
+            return_value = "Turning toward the cart";
+            break;
+        case PreApproachState::stop_turn:
+            return_value = "Stopping after turning toward the cart";
+            break;
+        case PreApproachState::forward_02:
+            return_value = "Moving toward the cart";
+            break;
+        case PreApproachState::stop_forward_02:
+            return_value = "Stopping inside the cart";
+            break;
+        case PreApproachState::elevator_up:
+            return_value = "Raising the elevator";
+            break;
+        case PreApproachState::stop:
+            return_value = "Arrived at the cart";
+            break;
+        default:
+            return_value = "Error: unknown robot state";
+            break;
+    }
+    return return_value;
+}
+
+const double PreApproach::kGoalAngularDisplacement = -(M_PI / 2.0);
+const std::string PreApproach::kIsGazeboParameter = "is_gazebo";
+
+} // namespace project_part2
